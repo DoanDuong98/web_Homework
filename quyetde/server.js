d04e23d4a89c8c253c9513c4f07222738d89fdde
duong080798 <duong080798@gmail.com>
2018-12-24 10:56:47 +0700

    BTVN5

---
+++ quyetde/server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express(); // app là web server
+
+app.use(express.static(__dirname+'/public/'));//
+app.use(bodyParser.urlencoded({extended:false}));
+app.get("/",(req,res)=>{
+   const questions = JSON.parse(fs.readFileSync("./question.json",{encoding:"utf-8"}));
+   if(questions.length==0){
+        res.send("Chưa có câu hỏi nào");
+   }else{
+        
+        const randomQuestion = questions[Math.floor(Math.random()*questions.length)]
+        res.send(`<h1>
+            ${randomQuestion.content}
+        </h1>
+        <form action="/countVote/${randomQuestion.id}" method="POST">
+            <button name="vote" value="yes">Đúng/Có/Phải</button>
+            <button name="vote" value="no">Sai/không/Trái</button>
+        </form>
+        `);
+   }
+   
+});
+// app.get("/about",(req,res)=>{
+//     res.sendFile(__dirname + "/resource/about.html");
+// }); // đến trang about
+app.post("/countVote/:idQues",(req,res)=>{
+    let vote = req.body.vote;
+    let idQues = req.params.idQues; // tham số id câu hỏi
+    const questions = JSON.parse(fs.readFileSync("./question.json",{encoding:"utf-8"})); // đọc dữ liệu ra
+    questions.forEach((question) => {
+        if(question.id == idQues){
+            if(vote=="yes"){
+                question.yes++;
+            }else if(vote=="no"){
+                question.no++;
+            }
+        }
+    });
+    fs.writeFileSync("./question.json",JSON.stringify(questions)); // ghi vào 
+    res.redirect("/")
+});
+app.get("/ask",(req,res)=>{
+    res.sendFile(__dirname + "/view/ask.html");
+}); // đến trang ask 
+
+app.post("/addQuestion",(req,res)=>{
+    const questions = JSON.parse(fs.readFileSync("./question.json",{encoding:"utf-8"})); // đọc dữ liệu ra
+    const newQuestion  = {
+        content: req.body.questionContent,
+        yes:0,
+        no:0,
+        id:questions.length
+    }; // dữ liệu mới
+    questions.push(newQuestion); // thêm dữ liệu vào json 
+    fs.writeFileSync("./question.json",JSON.stringify(questions)); // ghi vào 
+    res.redirect('/');
+});
+app.listen(6969,(err)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("Server start success!");
+    }
+});
